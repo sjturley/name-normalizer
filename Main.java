a47e97134839eb14bf1c994c60d3a43cef777b83
Steve Turley <steve@turleylabs.com>
2020-04-30 11:51:15 -0500

    Updates for repl.it

--- Main.java
+++
@@ -1,9 +0,0 @@
-import org.junit.*;
-import org.junit.runner.*;
-import static org.junit.Assert.*;
-
-public class Main {
-    public static void main(String[] args) {
-        org.junit.runner.JUnitCore.main("AuthorNameNormalizerTest");
-    }
-}
